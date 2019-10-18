860c91741d1830af534d379cc10822a02f604f42
nadchif <nadchif@gmail.com>
2019-10-18 18:51:53 +0200

    updated environment.ts for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCs3D36o_rhztQ7vqWCd_GbLHBbiw4S7bU',
+    authDomain: 'liteflix-bc18d.firebaseapp.com',
+    databaseURL: 'https://liteflix-bc18d.firebaseio.com',
+    projectId: 'liteflix-bc18d',
+    storageBucket: 'liteflix-bc18d.appspot.com',
+    messagingSenderId: '658769599478',
+    appId: '1:658769599478:web:2da496baf877e33f3085d7',
+    measurementId: 'G-SMX98TJKT0'
+  }
 };
